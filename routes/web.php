91414ed91b7b854f3d519575af0df0922af49419
root <root@mail.arunikasolusiinovasi.co.id>
2026-08-19 19:46:03 +0000

    Redirect root URL to login page

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PreviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('/preview', [PreviewController::class, 'index'])->name('preview');
 
